de2ea3bf921c6e7bafdc96ce1235d2eb1af760db
klashxx <klashxx@gmail.com>
2016-12-28 22:06:29 +0100

    docopt interface

--- cp_validator/main.py
+++ cp_validator/main.py
@@ -1,20 +1,41 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Validador de Códigos Postales
 """
+from __future__ import print_function
 
-
+from docopt import docopt, DocoptExit
+from schema import Schema, SchemaError
 from cp_validator.lib.extractor import get_postal
 
+def cli():
+    cli_docs = """cp_validator™ ... validación de códigos postales.
+Usage:
+  cp_validator <codigo> [options] ...
 
+Options:
+  --version            muestra la versión del programa y sale
+  -h, --help           muestra la ayuda y sale
+  """
 
-def main():
-    """Controlador"""
+    schema = Schema({'<codigo>': str,
+                     '--help': int,
+                     '--version': int})
 
-    postals = get_postal()
+    args = docopt(cli_docs)
+    try:
+        schema.validate(args)
+    except SchemaError:
+        raise DocoptExit
+
+    try:
+        print(args['<codigo>'])
+    except Exception as error:
+        raise DocoptExit('falla cp_validator: {0}'.format(str(error)))
 
     return None
 
+
 if __name__ == '__main__':
-    main()
+    cli()
