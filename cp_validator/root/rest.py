0240f632b77d77670618403cf03465847ad6d0ad
klashxx <klashxx@gmail.com>
2016-12-28 23:39:34 +0100

    entry point for single code

--- cp_validator/root/rest.py
+++ cp_validator/root/rest.py
@@ -2,7 +2,7 @@
 """
 RESTful API
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from cp_validator.root.extractor import get_postal
 
 postals = get_postal()
@@ -10,8 +10,14 @@ postals = get_postal()
 app = Flask(__name__)
 
 @app.route('/cp/api/v1.0/codigos', methods=['GET'])
-def get_tasks():
+def get_postals():
     return jsonify(postals)
+
+@app.route('/cp/api/v1.0/codigos/<codigo>', methods=['GET'])
+def get_cp(codigo):
+    if codigo not in postals:
+        abort(404)
+    return jsonify({codigo: postals[codigo]})
 
 if __name__ == '__main__':
     app.run(debug=True)
